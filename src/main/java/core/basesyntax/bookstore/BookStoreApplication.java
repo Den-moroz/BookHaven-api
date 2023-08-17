5cbe4faad947420706ef96f460e73ba3291045ac
Denis Moroz <135730971+Den-moroz@users.noreply.github.com>
2023-08-17 22:45:18 +0200

    added a new Book model and functionality for it (#1)
    * added a new Book model and functionality for it

--- src/main/java/core/basesyntax/bookstore/BookStoreApplication.java
+++ src/main/java/core/basesyntax/bookstore/BookStoreApplication.java
@@ -1,7 +1,12 @@
 package core.basesyntax.bookstore;
 
+import core.basesyntax.bookstore.model.Book;
+import core.basesyntax.bookstore.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -10,4 +15,16 @@ public class BookStoreApplication {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(BookService bookService) {
+        return args -> {
+            Book newBook = new Book();
+            newBook.setTitle("Kobzar");
+            newBook.setPrice(BigDecimal.valueOf(100));
+            newBook.setAuthor("Taras Schevchenko");
+            newBook.setIsbn("a1b");
+            bookService.save(newBook);
+            bookService.findAll().forEach(System.out::println);
+        };
+    }
 }
---
+++ src/main/java/core/basesyntax/bookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,51 @@
+package core.basesyntax.bookstore.repository.impl;
+
+import core.basesyntax.bookstore.model.Book;
+import core.basesyntax.bookstore.repository.BookRepository;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataAccessException("Can't create book " + book, e) {
+            };
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session
+                    .createQuery("FROM Book", Book.class)
+                    .getResultList();
+        }
+    }
+}
