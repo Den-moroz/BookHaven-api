df664717aa44dc685df68c0733957f5d1b5568f4
Denis Moroz <135730971+Den-moroz@users.noreply.github.com>
2023-09-12 17:30:08 +0200

    Add shopping cart, cart item and functionality for them (#9)
    * added new shopping cart model and functionality for it

--- src/main/java/core/basesyntax/bookstore/service/UserService.java
+++ src/main/java/core/basesyntax/bookstore/service/UserService.java
@@ -3,8 +3,11 @@ package core.basesyntax.bookstore.service;
 import core.basesyntax.bookstore.dto.user.UserRegistrationRequestDto;
 import core.basesyntax.bookstore.dto.user.UserRegistrationResponseDto;
 import core.basesyntax.bookstore.exception.RegistrationException;
+import core.basesyntax.bookstore.model.User;
 
 public interface UserService {
     UserRegistrationResponseDto register(UserRegistrationRequestDto request)
             throws RegistrationException;
+
+    User getAuthenticatedUser();
 }
